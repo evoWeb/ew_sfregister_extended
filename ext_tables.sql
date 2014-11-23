3d005cb691b021f024373a2f1add43bfe2eb5e04
Sebastian Fischer <typo3@evoweb.de>
2014-11-23 12:07:15 +0100

    Add sql and tca definition for added properties

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,14 @@
 # Table structure for table 'fe_users'
 #
 CREATE TABLE fe_users (
-	prefered_brand text NOT NULL
+	custom0 varchar(1) DEFAULT '',
+	custom1 varchar(1) DEFAULT '',
+	custom2 varchar(1) DEFAULT '',
+	custom3 varchar(1) DEFAULT '',
+	custom4 varchar(1) DEFAULT '',
+	custom5 varchar(1) DEFAULT '',
+	custom6 varchar(1) DEFAULT '',
+	custom7 varchar(1) DEFAULT '',
+	custom8 varchar(1) DEFAULT '',
+	custom9 varchar(1) DEFAULT ''
 );

492ddc9e617a0a0cff401ae68c5efdb57eb930ea
Sebastian Fischer <typo3@evoweb.de>
2017-08-12 10:43:47 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$extension = 'smartwt_sfregister_extended';
+$extension = 'ew_sfregister_extended';
 $model = \Evoweb\SfRegister\Domain\Model\FrontendUser::class;
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['sf_register']['extender'][$model][$extension] =
 	'EXT:ew_sfregister_extended/Classes/Domain/Model/FrontendUser.php';

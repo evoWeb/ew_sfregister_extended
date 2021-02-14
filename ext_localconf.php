521d83b9507fe95bbf4b63ee3155714ea6d7ae8b
Sebastian Fischer <typo3@evoweb.de>
2021-02-14 10:03:56 +0100

    Change extension key and update registration of extending model

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,10 @@
 <?php
 
+// Pre TYPO3 10
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['sf_register']['extender']['FrontendUser']['ew_sfregister_extended'] =
     'EXT:ew_sfregister_extended/Classes/Domain/Model/FrontendUser.php';
+
+// Post TYPO3 10
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['sf_register']['extender'][
+    \Evoweb\SfRegister\Domain\Model\FrontendUser::class
+]['ew_sfregister_extended'] = 'EXT:sf_register_extended/Classes/Domain/Model/FrontendUser.php';
